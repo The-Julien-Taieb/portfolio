76bfda4f31ca327d3cc0e4329477c5fd1755d09f
Julien Taieb <julien.taieb.pro@outlook.com>
2023-06-20 16:49:27 +0200

    Jour 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll(".boutonDuJeu");
+let monScore = 0;
+let scoreAdverse = 0;
+const score = document.querySelector("#score")
+
+
 
 for(let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function(){
@@ -6,16 +11,25 @@ for(let i = 0; i < buttons.length; i++) {
         const ordi = buttons[Math.floor(Math.random() * buttons.length)].innerHTML;
         let resultat = "";
         if ((joueur === "Pierre" && ordi === "Ciseaux") || (joueur === "Ciseaux" && ordi === "Feuille") || (joueur === "Feuille" && ordi === "Pierre")) {
-            resultat = "Bien joué ! Un point de plus"
+            resultat = "Bien joué ! Un point de plus";
+            monScore++;
         } else if (joueur === ordi) {
             resultat = "Egalité, recommencez !"
         } else {
-            resultat = "C'est perdu, dommage !"
-        }
+            resultat = "C'est perdu, un point pour l'ordi !"
+            scoreAdverse++;
+        };
         document.querySelector("#resultat").innerHTML = `
         Joueur : ${joueur} <br/>
         Ordinateur : ${ordi} <br/>
-        Résultat : ${resultat}
+        <br/>
+        ${resultat}
         `;
+        document.querySelector("#score").innerHTML = `
+        ${monScore} - ${scoreAdverse} 
+        `;
+        
+        
+        
     })
 }
